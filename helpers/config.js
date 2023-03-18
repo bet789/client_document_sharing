c0126b3aafbd9cd8f7abdf65237a577127931db4
Dee <deeit789@gmail.com>
2023-03-18 16:09:14 +0700

    Dee

--- helpers/config.js
+++ helpers/config.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   mediaServer: {
     API_URL: "https://media.bet789.mobi/api",
-    TOKEN: "Basic YWRtaW46ZjcxNDJmMmZjZDdkZTgzOTY4MmM0ZWExNGUxZWNhMDE=",
-    COLLECTIONID: "822d10ce-3ccc-401a-8da1-dee7a776e465",
+    TOKEN: "Basic aGV3aXQ6YTdlZmJiYWE1NTcyYzlkZmU4MWRkMzYzOGI1YmMzNDg=",
+    COLLECTIONID: "b4e0d3ef-6db5-410e-963f-12cf2ab49616",
     URL_IMAGE: "https://media.bet789.mobi/files/storage",
   },
 };
